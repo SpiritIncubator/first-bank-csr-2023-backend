d0d1d15cbac10f41182a93fa8b7f49283b3410c8
stan <tonytonitone6@gmail.com>
2023-10-19 17:18:52 +0800

    feat: add socket configure

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,27 @@
-import express, { Express, Request, Response, Application } from 'express';
+import express, { Application } from 'express';
+import { Server } from 'socket.io';
+import { createAdapter } from '@socket.io/redis-adapter';
+import { createServer } from 'http';
+import { createClient } from 'redis';
 import dotenv from 'dotenv';
+
 
 import router from './routes';
 
 dotenv.config();
 const app: Application = express();
 const port = process.env.PORT || 5000;
+const server = createServer(app);
 
-function initServer() {
+async function initServer() {
+  const pubClient = await createClient();
+  const subClient = pubClient.duplicate();
+  const io = new Server(server, {adapter: createAdapter(pubClient, subClient)});
   app.use('/api/v1', router);
-  app.listen(port, () => console.log(`Server is running on ${port} port!`));
+  
+  io.on("connection", (socket) => {})
+
+  server.listen(port, () => console.log(`Server is running on ${port} port!`));
 }
 
 export default initServer;
