8b3f3c3f08515b918ed00acf55c4d0fb7e878c40
Stan <tonytonitone6@gmail.com>
2023-10-28 23:54:19 +0800

    feat: add room func

--- src/server.ts
+++ src/server.ts
@@ -10,16 +10,34 @@ import router from './routes';
 
 dotenv.config();
 const app: Application = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3030;
 const server = createServer(app);
 
 async function initServer() {
-  const pubClient = await createClient();
-  const subClient = pubClient.duplicate();
-  const io = new Server(server, {adapter: createAdapter(pubClient, subClient)});
+  // const pubClient = await createClient();
+  // const subClient = pubClient.duplicate();
+  // , {adapter: createAdapter(pubClient, subClient)}
+  const io = new Server(server, {
+    cors: {
+      origin: '*',
+  }});
   app.use('/api/v1', router);
   
-  io.on("connection", (socket) => {})
+  io.on("connection", (socket) => {
+    console.log('client has connected to the server!');
+
+    socket.on('subscribeChannel', (args) => {
+      socket.join(args.room);
+    });
+
+    socket.on('sendRoomMsg', ({ room, message }) => {
+      io.to(room).emit(room, message);
+    });
+
+    socket.on('leaveRoom', ({ room }) => {
+      socket.leave(room);
+    })
+  })
 
   server.listen(port, () => console.log(`Server is running on ${port} port!`));
 }
