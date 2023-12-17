f99e2765a1c011cf26fcaa28fed72524f6026b1f
moojing <starts789@gmail.com>
2023-12-17 16:11:39 +0800

    feat: add allow cors for localhost 3000 port

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,20 @@ import { Server } from 'socket.io';
 import { createServer } from 'http';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
+import cors, { CorsOptions } from 'cors'
+
+var whitelist = ['http://localhost:3010']
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    console.log('origin :', origin);
+    if (!origin || whitelist.includes(origin)) {
+      console.log('whitelist :', whitelist);
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
 
 
 import router from './routes';
@@ -21,11 +35,13 @@ async function initServer() {
   const io = new Server(server, {
     cors: {
       origin: '*',
-  }});
+    }
+  });
 
   app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(cors(corsOptions));
   app.use('/api/v1', router);
-  
+
   io.on("connection", (socket) => {
     console.log('client has connected to the server!');
 
